73f63a72ee71657d1393c656b5831a2a69c634c3
kargbu <kargb@ciencias.unam.mx>
2024-09-18 16:07:42 -0300

    My commit message

--- clase_6/ejercicio_1.py
+++ clase_6/ejercicio_1.py
@@ -1,4 +1,4 @@
 # Definir una lista de números dada
-lista = [1,2,3,4,4,4,4,1,2,,5,6,6,7,7,8,9]
-lista_num = set(lista)
-print(lista_num)
+lis_num = [1,2,3,4,4,4,4,1,2,5,6,6,7,7,8,9]
+set_num = set(lis_num)
+print(set_num)
