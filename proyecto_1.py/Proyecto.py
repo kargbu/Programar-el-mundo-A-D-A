a0b840598ada3088884a7369c6dbf445c903ed05
kargbu <kargb@ciencias.unam.mx>
2024-09-21 11:22:01 -0300

    Últimas correcciones

--- proyecto_1.py/Proyecto.py
+++ proyecto_1.py/Proyecto.py
@@ -1,52 +1,90 @@
 # Este programa es un juego de Trivia sobre programación en Python.
 # Integrantes: 
 
-#Modulo random (paso 4)
-import random 
-print('¡Te doy la bienvenida a la Trivia sobre Python!')
-# Paso 3
-# Nombre usuario
-nombre = input('Ingresa tu nombre, por favor: ')
-# Edad usuario
-edad = int(input('Ingresa tu edad, por favor: '))
+# Paso 4 Modulo random
+import random
 
-if edad >= 18:
-    print(f' \n¡Hola {nombre}, Es hora de comenzar con la Trivia sobre python!\n ')
-    print("Antes de comenzar, las instrucciones del juego son las siguientes: ")
-    print('debes de contestar correctamente las preguntas, por cada respuesta correcta \nvas a ganar 1 punto. Sin emabrgo, por cada respuesta incorrecta \nno sumaras puntos, pero si te equivocas más de tres veces pierdes el juego.')
-    print('¡Ahora si, a jugar!')
-else:
-    print(f'Lo siento {nombre}, no tienes la edad mínima para jugar. ')
-    exit()
+# Paso 8 Creación de funciones
+def juego_trivia():
+
+    def bienvenida ():
+        print('\n¡Te doy la bienvenida al juego de L A   T R I V I A  D E   P Y T H O N!')
+        
+# Paso 3 Estructura básica del juego
+    # 3.1 Nombre del usuario
+
+    def datos ():
+        nombre = input('\nIngresa tu nombre, por favor: ')
+
+    # 3.2 Edad del usuario
+
+        edad = int(input('\nIngresa tu edad, por favor: '))
+       
+        if edad >= 18:
+         print(f'\n¡Hola, {nombre}!\nEres mayor de edad. Es hora de comenzar con la trivia.\n')
+        else:
+            print(f'\nLo siento {nombre}, no tienes la edad mínima para jugar. \n')
+            exit()
+        return nombre, edad
+
+    def instrucciones():
+        print('\nAntes de comenzar, las instrucciones del juego son las siguientes: debes contestar correctamente las preguntas. '
+              'Por cada respuesta correcta ganarás 1 punto. Sin embargo, por cada respuesta incorrecta no sumarás puntos, '
+              'pero si te equivocas más de tres veces, pierdes el juego.\n¡Ahora sí, a jugar!')
 
 # Paso 5 preguntas y respuestas
-# Definir las preguntas y sus respuestas en un diccionario
-preguntas = {
-    'Pregunta 1': 'verdadero',               
-    'Pregunta 2': 'verdadero',               
-    "Pregunta 3": 'falso'
+
+    # 5.1 Definir las preguntas y sus respuestas en un diccionario
+    def preguntas_y_respuestas():
+        preguntas = {
+        'Pregunta 1': 'verdadero',               
+        'Pregunta 2': 'verdadero',               
+        "Pregunta 3": 'falso'
  } # FALTA AÑADIR LAS PREGUNTAS!
 
-# Convertir en tuplas
-Lista_preguntas = list(preguntas.items())
+    # 5.2 Convertir en tuplas
 
-# Usar shuffle para mezclar las preguntas 
-random.shuffle(Lista_preguntas)
+        lista_preguntas = list(preguntas.items())
 
-# Preguntar y evaluar la respuesta
-score = 0
+    # 5.3 Usar shuffle para mezclar las preguntas 
 
-for preguntaX, respuesta_correcta in Lista_preguntas:     #Ciclo for para que me muestre las preguntas de manera aleatoria
-    respuesta_usuario = input(f'{preguntaX} (Responde verdadero o falso): ').lower()
+        random.shuffle(lista_preguntas)
+        return lista_preguntas
 
-    if (respuesta_usuario == 'verdadero' and respuesta_correcta) or (respuesta_usuario == 'falso' and not respuesta_correcta):
-        print('Es correcto')
-        score += 1
-    else:
-        print('Es incorrecto')
-    print(f'Tu puntuación es de {score}')
+# Paso 6 y 7 Mostrar la preguntar y evaluar la respuesta
+    # Contador de puntuación
+    def jugar(lista_preguntas):
+        score = 0
 
-print(f'Puntuación final: {score}/{len(Lista_preguntas)}')
+    # Contador de intentos fallidos
+        errores = 0
 
+    # 1.6 Ciclo para mostar preguntas
+        for preguntaX, respuesta_correcta in lista_preguntas:     #Ciclo for para que me muestre las preguntas de manera aleatoria
+            respuesta_usuario = input(f'{preguntaX} (Responde verdadero o falso): ').lower()
 
+# Validar respuestas
+        if (respuesta_usuario == 'verdadero' and respuesta_correcta) or (respuesta_usuario == 'falso' and not respuesta_correcta):
+            print('Es correcto')
+            score += 1
+        else:
+            print('Es incorrecto')
+            errores += 1
+            print(f'Tu puntuación es de {score}')
 
+    # 6.2 Contar y controlar los intentos fallidos
+        if errores != 3:
+            print(f'Puntuación final: {score}/{len(lista_preguntas)}')
+
+# Paso 7 Controlar errores consecutivos
+        while errores <= 3:
+            print(f'Fallaste tres veces. Final del juego.')
+            break
+    
+    bienvenida()
+    datos()
+    instrucciones()
+    lista_preguntas = preguntas_y_respuestas()
+    jugar(lista_preguntas)
+
+juego_trivia()
