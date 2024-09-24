c06cbbb4ec671a3a485ae90d4fe8ce92f80ee4b1
kargbu <kargb@ciencias.unam.mx>
2024-09-24 20:29:57 -0300

    va va va

---
+++ clase_11/ejemplos_modulos/math.py
@@ -0,0 +1,10 @@
+# importamos módulo
+import math
+
+# Constantes
+print(f'El valor de pi es: {math.pi}')
+
+# Calcular el seno de 90 grados
+angulo_grados = 90
+angulo_radianes = math.radianes(angulo_grados)
+print('f Seno de {angulo_grados} grados es: {math.sin(angulo_grados)}')
