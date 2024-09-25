29b2e84b25296ef8037e865805c4a061f55e98dd
kargbu <kargb@ciencias.unam.mx>
2024-09-24 21:01:24 -0300

    va

--- clase_11/ejemplos/random.py
+++ clase_11/ejemplos/random.py
@@ -1,8 +1,9 @@
+
 import random
 
 # Generar un número aleatorio flotante
 
 numero_aleatorio = random.random()
-# print(f'Número aleatorio (0 a 1): {numero_aleatorio}')
-print(numero_aleatorio)
+print(f'Número aleatorio (0 a 1): {numero_aleatorio}')
+# print(numero_aleatorio)
 
