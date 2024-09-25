d7f5bc2ec4db6faa1c14f31b176f2366db034eee
kargbu <kargb@ciencias.unam.mx>
2024-09-24 21:13:57 -0300

    va va va

--- clase_11/ejemplos/datetime.py
+++ clase_11/ejemplos/datetime.py
@@ -1,3 +1,21 @@
 # Importar el módulo
 import datetime
 
+# Fecha actual
+fecha_actual = datetime.date.today()
+print(f'Fecha actual: {fecha_actual}')
+
+# Hora actual
+hora_actual = datetime.datetime.now().time()
+print(f'Hora actual: {hora_actual}')
+
+# Fecha y hora actual
+fecha_hora_actual = datetime.datetime.now()
+print(f'La fecha y hora actual es {fecha_hora_actual}')
+
+# Crear una fecha y una hora específica
+mi_fecha = datetime.date(2024, 8, 22)
+mi_hora = datetime.time(14, 30, 0)
+
+print(f'Fecha específica: {mi_fecha}')
+print(f'Hora específica: {mi_hora}')
