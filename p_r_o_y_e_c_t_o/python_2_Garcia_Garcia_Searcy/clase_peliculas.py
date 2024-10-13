e992a4018edffb1bd296deca16201c9ceb1cfafe
kargbu <kargb@ciencias.unam.mx>
2024-10-13 19:58:49 -0300

    últimos cambios

--- p_r_o_y_e_c_t_o/python_2_Garcia_Garcia_Searcy/clase_peliculas.py
+++ p_r_o_y_e_c_t_o/python_2_Garcia_Garcia_Searcy/clase_peliculas.py
@@ -1,5 +1,4 @@
 
-# pelicula.py
 #  Esta clase se encargará de representar una película con sus atributos, como el nombre.
 
 
@@ -18,19 +17,13 @@ class Pelicula:
     @property
     def titulo(self):
         return self.__titulo
-# El decorador @property convierte el método titulo en un getter,
-#  permitiendo acceder al atributo __titulo de manera controlada.
 
-    # Setter con @nombre.setter
     @titulo.setter
     def titulo(self, nuevo_titulo):
         if isinstance(nuevo_titulo, str):
             self.__titulo = nuevo_titulo
         else:
             raise ValueError('El nombre debe ser una cadena de texto')
-# El decorador @titulo.setter define un setter para el atributo __titulo,
-#  permitiendo modificar su valor solo si nuevo_titulo es una cadena de texto.
-#  Si no lo es, se lanza un ValueError.
 
     @property
     def anio(self):
@@ -43,16 +36,3 @@ class Pelicula:
         else:
             raise ValueError('El año de la película debe ser un número')
 
-
-
-# Uso de la propiedad
-#pelicula_1 = Pelicula('Matriz', 1999, 'Ciencia Ficción', 'Lana Wachowski')
-#print(pelicula_1.titulo)  # Llama al getter sin usar paréntesis
-#pelicula_1.titulo = 'Matrix'  # Llama al setter como una asignación directa
-#print(pelicula_1.titulo)  # Accede nuevamente al atributo usando getter}
-
-#pelicula_2 = Pelicula('Una familia normal', 2003, 'Drama', 'Per Hanefjord') 
-# La pelicula_2 tiene el año erroneo, debe ser 2023
-#pelicula_2.mostrar_info() # Mostrar la información
-
-# ¿Cómo se accede al año de película para cambiar el año?
